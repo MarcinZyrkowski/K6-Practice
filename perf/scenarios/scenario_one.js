c6aa049490beb4925f5209413b7ed61e639e37f9
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2024-03-15 19:33:44 +0100

    created own metric

--- perf/scenarios/scenario_one.js
+++ perf/scenarios/scenario_one.js
@@ -1,17 +1,23 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 import { check } from 'k6';
+import { Counter } from 'k6/metrics';
 
 export const options = {
-  vus: 10, 
-  duration: '5s',
+  vus: 5, 
+  duration: '3s',
   // defining thresholds
   thresholds: {
-    http_req_duration: ['p(90)<200'], // 95% of requests should take less or equal than 500
+    http_req_duration: ['p(90)<2000'], // 95% of requests should take less or equal than 2000
     http_req_failed: ['rate<0.05'], // only 5% of tests can fail
-    http_reqs: ['count>10']
+    http_reqs: ['count>10'],
+    checks: ['rate>=0.99'],
+    my_counter: ['count>5']
   }
 };
+
+// creating onw metric of type Counter
+let myCounter = new Counter('my_counter');
 
 // standardChecks is an object
 export const standardChecks = {
@@ -21,6 +27,8 @@ export const standardChecks = {
 
 export default function () {
   const res = http.get('http://test.k6.io');
+  // defining logic to custom metric
+  myCounter.add(1);
   sleep(1);
 
   // trivial check
@@ -31,4 +39,3 @@ export default function () {
   // res is dynamic value
   check(res, standardChecks);
 }
-
